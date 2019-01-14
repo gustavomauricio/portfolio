6ca4e1f3ca4982cef041a5da053efa1938abbd5f
Gustavo Mauricio <gustavo.mauricio58@gmail.com>
2019-01-14 23:08:01 +0000

    last nav link highlighted when scroll is at bottom. font size responsive for small devices

--- js/main.js
+++ js/main.js
@@ -54,7 +54,11 @@ $(document).ready(function() {
     var scroll = $(window).scrollTop();
         $('.nav-link').each(function(){
         var href = $(this).attr('href');
-            if(scroll > $(href).offset().top - 300 && scroll < $(href).offset().top + $(href).outerHeight()){
+            if(scroll + window.innerHeight === document.body.offsetHeight){
+                $('.nav-link').removeClass('color-orange');
+                $("a[href*='contact']").addClass('color-orange');
+            }
+            if(scroll > $(href).offset().top - 1 && scroll < $(href).offset().top + $(href).outerHeight()){
                 $('.nav-link').removeClass('color-orange');
                 $(this).addClass('color-orange');
             }
